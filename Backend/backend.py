927a9babf717b261c022f5f802a8444fceb11ecc
josefmb <josefmb17@gmail.com>
2020-07-02 17:08:44 -0300

    backend espero q n de erro mais

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,16 +1,16 @@
 from config import *
-from time import Time
+from models import Time
 
 @app.route("/")
 def inicar():
     return 'Sistema de cadastro de times. '+\
-        '<a href="/listar_pessoas">Operação listar</a>'
+        '<a href="/listar_times">Operação listar</a>'
 
 @app.route("/listar_times")
 def listar_times():
     times = db.session.query(Time).all()
     # aplicar o método json que a classe Time possui a cada elemento da lista
-    times_em_json = [ x.json() for x in pessoas ]
+    times_em_json = [ x.json() for x in times ]
     # fornecer a lista de times em formato json
     return jsonify(times_em_json)
 
--- Backend/models.py
+++ Backend/models.py
@@ -1,10 +1,10 @@
 from config import *
 
 class Time(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(254))
     esporte = db.Column(db.String(254))
-
     #Método para expressar o time em formto texto
     
     def __str__(self):
@@ -30,14 +30,16 @@ if __name__ == "__main__":
     # teste da classe Pessoa
     t1 = Time(nome = "FC Bayern München", esporte = "Futebol")
     t2 = Time(nome = "CR Flamengo", esporte = "Futebol")        
-    
+
     # persistir
     db.session.add(t1)
     db.session.add(t2)
     db.session.commit()
     
     # exibir a pessoa
+    print(t1)
     print(t2)
 
     # exibir a pessoa no format json
+    print(t1.json())
     print(t2.json())
